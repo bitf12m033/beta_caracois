304da439260c18188d6f29443a8a911963d8001e
bitf12m033 <umermj2@gmail.com>
2020-09-19 06:56:48 +0200

    Email changes

--- config/mail.php
+++ config/mail.php
@@ -36,11 +36,11 @@ return [
     'mailers' => [
         'smtp' => [
             'transport' => 'smtp',
-            'host' => env('MAIL_HOST', 'smtp.mailgun.org'),
+            'host' => env('MAIL_HOST', 'smtp.mailtrap.io'),
             'port' => env('MAIL_PORT', 587),
             'encryption' => env('MAIL_ENCRYPTION', 'tls'),
-            'username' => env('MAIL_USERNAME'),
-            'password' => env('MAIL_PASSWORD'),
+            'username' => env('MAIL_USERNAME','7b3d87f42d19fb'),
+            'password' => env('MAIL_PASSWORD','a033232726dc51'),
             'timeout' => null,
             'auth_mode' => null,
         ],
